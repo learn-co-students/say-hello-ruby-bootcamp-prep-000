0c8d08bc7ef8761c3755def2248a17e2444de544
Megan McLean <mmclean@protonmail.com>
2017-01-13 06:29:08 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,9 @@
-# Build your say_hello method here
+# Learn.co Bootcamp Prep - Ruby Fundamentals - 08: Say Hello
+
+# Define a method, 'say_hello', that accepts one parameter(s):
+# 'name' with default value 'Ruby Programmer'.
+# Print "Hello <name>!"
+
+def say_hello(name="Ruby Programmer")
+  puts "Hello #{name}!"
+end
